7e3e58877da7239fe82328614cfa89f35bff421d
MohaK <mohakhalili@outlook.com>
2025-05-03 16:00:54 +0200

    added auth and users endpoints

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import exercises from "./routes/exercises";
 import exercisesinfo from "./routes/exercisesinfo";
 import reviews from "./routes/reviews";
+import users from "./routes/users";
+import auth from "./routes/auth";
 import cors from "cors";
 
 const app = express();
@@ -11,6 +13,8 @@ app.use(express.json());
 app.use("/api/exercises", exercises);
 app.use("/api/exercisesinfo", exercisesinfo);
 app.use("/api/reviews", reviews);
+app.use("/api/users", users);
+app.use("/api/auth", auth);
 
 const PORT = 6688;
 
